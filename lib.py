11e7caa5ce8b8fc61d74996b3a4bc5a43eaabe4c
socean <lumengeared@gmail.com>
2022-10-20 20:03:30 +0800

    idk

--- lib.py
+++ lib.py
@@ -1,8 +1,7 @@
 # Year 8 Math thingo (much harder than Year 9 for some reason)
 # Lodinu Kalugalage
-# ethan xu drool
 
-from symtable import Symbol
+import math
 
 
 INPUT_QUERIES = [
@@ -19,16 +18,24 @@ IQ_TRI_2_ANG = 3
 
 TRI_NAN = "NA"
 
+TRI_LENGTH_AB = 0
+TRI_LENGTH_AC = 1
+TRI_LENGTH_BC = 2
+
+TRI_ANGLE_BAC = 0
+TRI_ANGLE_ABC = 1
+TRI_ANGLE_ACB = 2
+
 # triangle shape
 #
-#    A
-#    / \\
-#    /   \\
-#    /     \\ AC
-# AB /       \\
-#    /         \\
-#    /           \\
-#    B-------------C
+#          A
+#         /\
+#        /  \
+#       /    \ AC
+# AB   /      \
+#     /        \
+#    /          \
+#    B-----------C
 #        BC
 
 
@@ -63,7 +70,7 @@ def validate_triangle_lengths(lengths):
         return True
     longest_side = max(lengths)
     if longest_side >= sum(lengths) - longest_side:
-        return False
+        return "The longest side is longer than the sum of the other two sides"
     return True
 
 
@@ -71,8 +78,13 @@ def validate_triangle_angles(angles):
     # Check if the angles are valid
     if TRI_NAN in angles:
         return True
-    if sum(angles) != 180:
-        return False
+    s = sum(filter(lambda x: x != TRI_NAN, angles))
+    nan_count = angles.count(TRI_NAN)
+    if nan_count == 0:
+        if s != 180:
+            return "The sum of the angles is not 180"
+    if s > 180:
+        return "The sum of the angles is greater than 180"
     return True
 
 
@@ -115,15 +127,86 @@ class Triangle:
 
 
 def compute_unknown_angle(tri: Triangle):
-    nan_count = tri.angles.count(TRI_NAN)
+    rvalue = tri.angles.copy()
+    nan_locations = []
+    for i in range(3):
+        if rvalue[i] == TRI_NAN:
+            nan_locations.append(i)
+    nan_count = len(nan_locations)
     if nan_count == 0:
-        return tri.angles.copy()
+        pass
     elif nan_count == 1:
-        rvalue = tri.angles.copy()
-        sum_wo_nan = sum([i for i in tri.angles if i != TRI_NAN])
-        rvalue[rvalue.index(TRI_NAN)] = 180 - sum_wo_nan
-        return rvalue
+        rvalue[nan_locations[0]] = 180 - \
+            sum(filter(lambda x: x != TRI_NAN, rvalue))
+    elif len(list(filter(lambda x: x != TRI_NAN, tri.lengths))) == 3:
+        # (a^2 + c^2 - b^2) / 2ac
+        cos_1 = ((float(tri.lengths[TRI_LENGTH_AC]) ** 2) +
+                 (float(tri.lengths[TRI_LENGTH_AB]) ** 2) - (float(tri.lengths[TRI_LENGTH_BC]) ** 2)) \
+            / (2 * float(tri.lengths[TRI_LENGTH_AC]) * float(tri.lengths[TRI_LENGTH_AB]))
+
+        # (b^2 + c^2 - a^2) / 2bc
+        cos_2 = ((float(tri.lengths[TRI_LENGTH_BC]) ** 2) +
+                 (float(tri.lengths[TRI_LENGTH_AB]) ** 2) - (float(tri.lengths[TRI_LENGTH_AC]) ** 2)) \
+            / (2 * float(tri.lengths[TRI_LENGTH_BC]) * float(tri.lengths[TRI_LENGTH_AB]))
+
+        # (a^2 + b^2 - c^2) / 2ab
+        cos_3 = (float(tri.lengths[TRI_LENGTH_AC] ** 2) +
+                 (float(tri.lengths[TRI_LENGTH_BC]) ** 2) - (float(tri.lengths[TRI_LENGTH_AB]) ** 2)) \
+            / (2 * float(tri.lengths[TRI_LENGTH_AC]) * float(tri.lengths[TRI_LENGTH_BC]))
+
+        rvalue = [round(math.degrees(math.acos(cos_1))), round(math.degrees(
+            math.acos(cos_2))), round(math.degrees(math.acos(cos_3)))]
+
+    tri.angles = rvalue
+    return
+
+
+def __sin_rule(C, D):
+    if C == TRI_NAN or D == TRI_NAN:
+        return (False, TRI_NAN)
+    return (True, D / (math.sin(math.radians(C))))
+
+
+def compute_unknown_side(tri: Triangle):
+    rvalue = tri.lengths.copy()
+    nan_locations = []
+    for i in range(3):
+        if rvalue[i] == TRI_NAN:
+            nan_locations.append(i)
+    nan_count = len(nan_locations)
+    if nan_count == 0:
+        return
+
+    # use the cosine rules to get side lengths from angles
+    # using this because we need angles
+    compute_unknown_angle(tri)
+    sine_rule = {TRI_ANGLE_BAC: __sin_rule(
+        tri.angles[TRI_ANGLE_BAC], tri.lengths[TRI_LENGTH_BC]),
+        TRI_ANGLE_ABC: __sin_rule(
+        tri.angles[TRI_ANGLE_ABC], tri.lengths[TRI_LENGTH_AC]),
+        TRI_ANGLE_ACB: __sin_rule(
+        tri.angles[TRI_ANGLE_ACB], tri.lengths[TRI_LENGTH_AB])}
+    # get the first occurence with a valid value
+    sine_rule_value = TRI_NAN
+    for i in range(3):
+        if sine_rule[i][0]:
+            sine_rule_value = sine_rule[i][1]
+            break
+    if sine_rule_value == TRI_NAN:
+        return
+
+    for i in range(3):
+        if rvalue[i] == TRI_NAN:
+            rvalue[i] = int(sine_rule_value *
+                            (math.sin(math.radians(tri.angles[i]))))
+
+    tri.lengths = rvalue
+    return
 
 
 if __name__ == "__main__":
-    print(get_triangles())
+    # t = get_triangles()
+    tri_1 = Triangle([6, TRI_NAN, 6], [60, TRI_NAN, TRI_NAN])
+    # tri_2 = Triangle(t[2], t[3])
+    compute_unknown_side(tri_1)
+    print(tri_1)
